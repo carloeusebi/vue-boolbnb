6cac457e6f306b3591180595d986a8b99bf41996
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-26 12:10:14 +0200

    logic for sponsor apartments add

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import {router} from './routes'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).use(router).mount('#app')
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,28 @@
 <script>
+import axios from 'axios';
+const endpoint = 'http://localhost:8000/api/apartments/';
+
 export default {
 	name: 'HomePage',
+
+	data() {
+		return {
+			notSponsorApartments: [
+
+			],
+		};
+	},
+	created() {
+		axios.get(endpoint).then(res => {
+
+			const apartments = res.data;
+
+			this.notSponsorApartments = apartments.filter(apartment => !apartment.sponsored);
+
+			console.log(this.notSponsorApartments);
+
+		});
+	}
 };
 </script>
 
