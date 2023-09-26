910fee9aa6c307ed219619398d7189ec3ce8fe44
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-26 13:03:41 +0200

    Index complete

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -19,16 +19,60 @@ export default {
 
 			this.notSponsorApartments = apartments.filter(apartment => !apartment.sponsored);
 
-			console.log(this.notSponsorApartments);
-
 		});
+
 	}
 };
 </script>
 
 <template>
-	temporary page
-	<div>
-		<RouterLink :to="{ name: 'apartments.create' }">Create Apartment</RouterLink>
+	<div class="container mx-auto p-0 pb-5">
+
+		<h1 class="text-center fw-bold mt-4">temporary page</h1>
+		<hr>
+		<div class="d-flex justify-content-center align-items-center">
+			<RouterLink :to="{ name: 'apartments.create' }">Create Apartment</RouterLink>
+		</div>
+		<hr>
+
+
+		<!-- CARDS: -->
+		<div class="container pb-5 p-0 m-0">
+
+			<h2 class="text-center text-secondary  pt-3 pb-3">Apartments:</h2>
+
+			<div class="cards_deck row justify-content-center gap-5 m-0 p-0">
+				<div class="card_container" v-for="apartment in notSponsorApartments" :key="apartment.id">
+					<div class="col-12 col-md-10 col-lg-8">
+						<div style="width: max-content;" class="card">
+							<img :src="`http://localhost:8000/storage/${apartment.thumbnail}`" class="image-fluid"
+								:alt="apartment.name">
+							<div class="card-body">
+								<h5 class="card-title">{{ apartment.name }}</h5>
+								<div class="d-flex justify-content-between align-items-center">
+									<a href="#">View...</a>
+									<!--<RouterLink :to="{ name: 'ApartmentDetailPage', params: { slug: apartment.slug } }">
+										View...</RouterLink>-->
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+
 	</div>
 </template>
+
+<style scoped>
+.cards_deck .card_container {
+	width: fit-content;
+	max-width: 300px;
+}
+
+.cards_deck {
+	max-width: 1200px;
+}
+</style>
