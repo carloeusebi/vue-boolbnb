73fdb00f0bcc66b2283de62a35d72d21fe8150cc
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-27 17:01:10 +0200

    cards positioned perfectly in the center

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -38,10 +38,10 @@ export default {
 
 		<h2 class="text-center text-secondary  pt-3 pb-3">Apartments:</h2>
 		<!-- CARDS: -->
-		<div class="d-flex justify-content-center  container bg-light rounded-1 pb-5 p-0 m-0">
+		<div class="d-flex justify-content-center bg-light rounded-1 pt-4 pb-5 p-0 m-0">
 
-			<div class="cards_deck row justify-content-center gap-5 m-0 p-0">
-				<div class="card_container" v-for="apartment in notSponsorApartments" :key="apartment.id">
+			<div class="cards_deck row justify-content-center justify-content-lg-start gap-5 m-0 p-0">
+				<div class="card_container p-0" v-for="apartment in notSponsorApartments" :key="apartment.id">
 					<div class="col-12 col-md-10 col-lg-8">
 						<div style="width: 300px;" class="card">
 							<img :src="`http://localhost:8000/storage/${apartment.thumbnail}`" class="img-fluid"
@@ -66,11 +66,10 @@ export default {
 
 <style scoped>
 .cards_deck {
-	max-width: 1200px;
+	max-width: 1005px;
 }
 
 .cards_deck .card_container {
-	width: fit-content;
 	max-width: 300px;
 }
 
