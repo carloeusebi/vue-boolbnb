ac5d0f1153e1893a03863486d6cbee643f9d3fff
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-10-06 12:02:29 +0200

    header style fix

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -16,7 +16,7 @@ export default {
     <header>
 
         <!-- navbar -->
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg">
             <div class="container">
                 <!-- logo -->
                 <RouterLink class="navbar-brand" :to="{ name: 'home' }">
@@ -29,10 +29,11 @@ export default {
                 <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
                     <!-- left side -->
                     <ul class="navbar-nav">
-                        <li v-for="link in links" :key="link.to" class="nav-item">
-                            <RouterLink class="nav-link active" :to="{ name: link.to }">
+                        <li v-for="link in links" :key="link.to" class="me-3 nav-item">
+                            <RouterLink id="nav_color" class="nav-link active" :to="{ name: link.to }">
                                 {{ link.label }}
                             </RouterLink>
+                            <div id="bottom_nav_link"></div>
                         </li>
                     </ul>
 
@@ -46,5 +47,41 @@ export default {
 #logo {
     max-width: 80px;
     border-radius: 50%;
+    border: solid #f2c14e;
 }
-</style>
+
+header {
+    background-color: #faedcd;
+    font-weight: 800;
+    font-size: 24px;
+    letter-spacing: 1px;
+    border-bottom: solid #f2c14e;
+    -webkit-box-shadow: 0px 0px 17px 10px #f2c14e;
+    box-shadow: 0px 0px 17px 10px #f2c14e;
+}
+
+#nav_color {
+    color: #296a4f;
+}
+
+.active {
+    color: #1b4332;
+}
+
+#bottom_nav_link {
+    margin: auto;
+    height: 5px;
+    width: 0%;
+    border-radius: 5px;
+    background-color: #296a4f;
+    transition: width 0.5s;
+}
+
+/* PSEUDO-CLASSES: */
+#nav_color:hover+#bottom_nav_link {
+    width: 95%;
+}
+
+#nav_color:active {
+    color: #1b4332;
+}</style>
